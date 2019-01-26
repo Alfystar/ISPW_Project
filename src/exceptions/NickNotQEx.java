36e8fef28f31d209175594e201d368a9d936a165
filippo <Paciotti4us>
2019-01-26 18:47:03 +0100

    FIL [START] Exceptions

--- src/exceptions/NickNotQEx.java
+++ src/exceptions/NickNotQEx.java
@@ -2,8 +2,10 @@ package exceptions;
 
 public class NickNotQEx extends Exception{
 
+    private static final long serialVersionUID = 1000001L;
+
     public NickNotQEx(String message ){ super(message); }
     public NickNotQEx(Throwable cause ){super(cause);}
-    public NickNotQEx(String message, Throwable cause ){ super ("+++" + message + "+++" + cause); }
+    public NickNotQEx(String message, Throwable cause ){ super ("++MEX: " + message + "++CAUSE: " + cause); }
 
 }
