41ef0abc3401c513b900ecda98dd1707b53eee4a
alfy <alfystar1701@gmail.com>
2019-01-26 18:46:46 +0100

    login grafico

--- src/gluonBoundary/ControllerLogin.java
+++ src/gluonBoundary/ControllerLogin.java
@@ -1,4 +1,13 @@
 package gluonBoundary;
 
 public class ControllerLogin {
+    public void loginPush()
+    {
+        System.out.println("login");
+    }
+
+    public void registerPush()
+    {
+        System.out.println("register");
+    }
 }
