b0437eab8addb92db53b94262c7ce7f9497721f9
filippo <Paciotti4us>
2019-01-27 12:48:44 +0100

    FIL [ALMOST] Questions, added a lot of methods

--- src/entity/Questions.java
+++ src/entity/Questions.java
@@ -1,15 +1,62 @@
 package entity;
 
-import java.util.Vector;
+import static java.lang.Integer.max;
 
 public class Questions {
-    private Vector<String> answers = new Vector<String>();
 
-    public Questions() {
+    private String[] questions = {
+            "What is the name of your favorite pet?",
+            "Who is your favorite actor, musician, or artist?",
+            "What is your favorite movie?",
+            "What is the name of your first school?"
+    };
 
+    private String[] answers = {};
+
+    public Questions(String[] answers) {
+
+        this.answers = answers;
     }
 
-    public Questions(Questions questions){
-        //todo: Da continuare
+    public Questions(Questions q){
+        this.answers = q.getAnswers();
+        this.questions = q.getQuestions();
     }
+
+    /*  VERIFICA CHE ALMENO n DOMANDE SIANO CORRETTE*/
+
+    public Boolean compareAnswers(Questions q, int n){
+
+        String[] tmpAns = q.getAnswers();
+
+        int minCorrect = max(n, tmpAns.length);
+
+        int correct = 0;
+
+        for(int i = 0; i < tmpAns.length; i++){
+
+            if ( this.answers[i].equals(tmpAns[i]) ) correct += 1;
+        }
+
+        return (correct >= minCorrect);
+    }
+
+    public void changeAnswers(Questions q){
+
+        this.answers = q.getAnswers();
+    }
+
+    public void changeAnAnswer(Questions q, int index){
+
+        this.answers[index] = q.getAnswers()[index];
+    }
+
+    private String[] getAnswers(){
+        return this.answers;
+    }
+
+    private String[] getQuestions(){
+        return this.questions;
+    }
+    //todo: rivedere Class Diagram in UML (aggiunta di un po' di metodi)
 }
