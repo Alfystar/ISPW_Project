543f46cf7c5f6d7a9ce154d1813b97f13a079e20
alfy <alfystar1701@gmail.com>
2019-02-07 12:01:07 +0100

    user pane dinamico

---
+++ src/gluonBoundary/utilityClass/BeanLogUs.java
@@ -0,0 +1,15 @@
+package gluonBoundary.utilityClass;
+
+import entity.Nickname;
+
+public class BeanLogUs {
+    private Nickname nick;
+
+    public Nickname getNick() {
+        return nick;
+    }
+
+    public void setNick(Nickname nick) {
+        this.nick = nick;
+    }
+}
