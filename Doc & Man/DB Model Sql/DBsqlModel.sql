c7fc0d14318ec563a9ae3faab949a71bd62580aa
alfy <alfystar1701@gmail.com>
2019-02-08 17:26:46 +0100

    sql corretto

--- Doc & Man/DB Model Sql/DBsqlModel.sql
+++ Doc & Man/DB Model Sql/DBsqlModel.sql
@@ -127,4 +127,3 @@ USE `User`;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-deletesessionanswersprivatedatauseruserpublicdatauser
