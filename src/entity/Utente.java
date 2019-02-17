3f74abf70043f4b316d497a2d1238ce3cdf1fc75
alfy <alfystar1701@gmail.com>
2019-02-17 11:43:15 +0100

    ENTITÀ THREAD SAFE

--- src/entity/Utente.java
+++ src/entity/Utente.java
@@ -1,7 +1,5 @@
 package entity;
 
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class Utente{
     private BasicUserInfo pubD;
