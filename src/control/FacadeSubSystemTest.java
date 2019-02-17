7d192df3058c5f6db066540a36788146eefc0d39
alfy <alfystar1701@gmail.com>
2019-02-17 15:47:51 +0100

    code coverege

--- src/control/FacadeSubSystemTest.java
+++ src/control/FacadeSubSystemTest.java
@@ -4,6 +4,7 @@ import bean.UserInfoRegister;
 import entity.*;
 import exceptions.UserNotExistEx;
 import gluonBoundary.utilityClass.RandomString;
+import org.junit.jupiter.api.*;
 
 import java.util.GregorianCalendar;
 import java.util.Random;
@@ -21,7 +22,7 @@ class FacadeSubSystemTest{
     private Utente lastRandomUser;
 
 
-    @org.junit.jupiter.api.BeforeEach
+    @BeforeEach
     void setUp() throws Exception{
         FacadeSubSystemTest ft = new FacadeSubSystemTest();
         lastRandomUser = ft.randomUser();
@@ -30,29 +31,29 @@ class FacadeSubSystemTest{
 
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void getBasicUserInfo()throws Exception{
         BasicUserInfo usInfo = facade.getBasicUserInfo(lastRandomUser.getPublic().getNick());
-        //assertEquals(factInf.createBasic(lastRandomUser.getPublic()),usInfo,"Test uguaglianza dei BasicUserInfo");
+        assertEquals(lastRandomUser.getPublic().clone(),usInfo,"Test uguaglianza dei BasicUserInfo");
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void getRestrictedUserInfo()throws Exception{
         RestrictUserInfo usInfo = facade.getRestrictedUserInfo(lastRandomUser.getPublic().getNick());
-        //assertEquals(factInf.createRestrict(lastRandomUser.getPrivate()),usInfo,"Test uguaglianza dei RestrictUserInfo");
+        assertEquals(lastRandomUser.getPrivate().clone(),usInfo,"Test uguaglianza dei RestrictUserInfo");
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void doesNicknameExist()throws Exception{
         assertTrue(facade.doesNicknameExist(lastRandomUser.getPublic().getNick()),"Test doesNicknameExist");
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void doesTaxCodeExist()throws Exception{
         assertTrue(facade.doesTaxCodeExist(lastRandomUser.getPublic().getTC()),"Test doesTaxCodeExist");
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void createUser()throws Exception{
         UserInfoRegister infoReg = randomInfoReg();
         facade.createUser(infoReg.getNickname(),infoReg);
@@ -76,13 +77,13 @@ class FacadeSubSystemTest{
         System.out.println("Se gli altri hanno avuto successo è evidente che questo metodo funziona");
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void cancelUser()throws Exception{
         facade.cancelUser(lastRandomUser.getPublic().getNick());
         assertEquals(lastRandomUser.getStatus(),userExpert.getUser(lastRandomUser.getPublic().getNick()).getStatus(),"Test cancelUser cambia lo status a CANCELLED");
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void deleteUser()throws Exception{
         facade.deleteUser(lastRandomUser.getPublic().getNick());
         assertThrows(UserNotExistEx.class,()->{userExpert.getUser(lastRandomUser.getPublic().getNick());}, "Eliminazione dell'utente dal sistema istantaneamente");
@@ -90,30 +91,69 @@ class FacadeSubSystemTest{
 
 
 
-    @org.junit.jupiter.api.Test
+    @Test
     void validate()throws Exception{
         assertTrue(facade.validate(lastRandomUser.getPublic().getNick(),lastRandomUser.getPw()));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void checkQuestion()throws Exception{
         assertTrue(facade.checkQuestion(lastRandomUser.getPublic().getNick(),lastRandomUser.getQuestionary()));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void getAvatar()throws Exception{
         assertEquals(lastRandomUser.getPublic().getAvatar(),facade.getAvatar(lastRandomUser.getPublic().getNick()));
     }
 
 
-    @org.junit.jupiter.api.Test
+    @Test
     void getRoles()throws Exception{
         assertEquals(lastRandomUser.getRole(),facade.getRoles(lastRandomUser.getPublic().getNick()));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void getStatus()throws Exception{
         assertEquals(lastRandomUser.getStatus(),facade.getStatus(lastRandomUser.getPublic().getNick()));
+    }
+    
+    @AfterAll
+    static void coveregTest()throws Exception{
+        FacadeSubSystem facade = new FacadeSubSystem();
+        FacadeSubSystemTest ft = new FacadeSubSystemTest();
+        Utente lastRandomUser = ft.randomUser();
+        ft.userExpert.createUserAll(lastRandomUser);
+
+        facade.doesNicknameExist(lastRandomUser.getPublic().getNick()); //branch ok
+        facade.doesNicknameExist(new Nickname(ft.randomString()));         //branch false
+        facade.doesTaxCodeExist(lastRandomUser.getPublic().getTC());    //branch ok
+        facade.doesTaxCodeExist(new TaxCode(ft.randomString()));           //branch false
+        facade.changeNotAnagraphicData(lastRandomUser.getPublic().getNick(),new SocialStatus(ft.randomString()));      //branch ok
+        facade.changeNotAnagraphicData(lastRandomUser.getPublic().getNick(),new Email(ft.randomString()));             //branch ok
+        facade.changeNotAnagraphicData(lastRandomUser.getPublic().getNick(),new SurfaceAddress(ft.randomString()));    //branch ok
+        facade.changeNotAnagraphicData(lastRandomUser.getPublic().getNick(),new Nationality(ft.randomString()));       //branch ok
+        facade.changeNotAnagraphicData(lastRandomUser.getPublic().getNick(),new PhoneNumber(ft.randomString()));       //branch ok
+        //facade.changeNotAnagraphicData(new Nickname(ft.randomString()),new Email(ft.randomString()));                     //branch false
+        facade.changePassword(lastRandomUser.getPublic().getNick(),lastRandomUser.getPw(),new PW(ft.randomString()));
+        //facade.changePassword(new Nickname(ft.randomString()),lastRandomUser.getPw(),new PW(ft.randomString()));
+        facade.forgottenPassword(lastRandomUser.getPublic().getNick(),lastRandomUser.getQuestionary(),new PW(ft.randomString()));
+        PW newPw = new PW(ft.randomString());
+        //facade.changePassword(new Nickname(ft.randomString()),newPw,newPw);
+        facade.changeHost("localHost");
+        facade.getLastHost();
+        facade.setAvatar(lastRandomUser.getPublic().getNick(),1);
+        facade.makeARenter(lastRandomUser.getPublic().getNick());
+        facade.removeRentership(lastRandomUser.getPublic().getNick());
+        facade.makeATenant(lastRandomUser.getPublic().getNick());
+        facade.removeTenantship(lastRandomUser.getPublic().getNick());
+        facade.changeUserStatus(lastRandomUser.getPublic().getNick(),UserStatus.ACTIVE);
+        facade.removeTenantship(lastRandomUser.getPublic().getNick());
+        facade.isRenter(lastRandomUser.getPublic().getNick());
+        facade.isTenant(lastRandomUser.getPublic().getNick());
+        facade.isActive(lastRandomUser.getPublic().getNick());
+        facade.isInactive(lastRandomUser.getPublic().getNick());
+        facade.isCancelled(lastRandomUser.getPublic().getNick());
+        facade.isBanned(lastRandomUser.getPublic().getNick());
     }
 
     private Utente randomUser()
@@ -123,6 +163,8 @@ class FacadeSubSystemTest{
         Utente us = new Utente(pubD, priD, new PW(randomString()), new Roles(randBoolean(), randBoolean()), new Questionary(new String[]{randomString(),randomString(),randomString(),randomString()}));
         return us;
     }
+
+
 
     private UserInfoRegister randomInfoReg() {
         String[] answ= {randomString(), randomString(),randomString(),randomString()};
@@ -152,5 +194,4 @@ class FacadeSubSystemTest{
         if(randomNum >= 500) return Gender.MAN;
         else return Gender.WOMAN;
     }
-
 }
